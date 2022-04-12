782aceab6606bef286a262f07ade7e1bb9ba45d0
wendko <wendko.88@gmail.com>
2022-04-13 02:26:46 +0800

    fix: caught logic

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const LOCAL_STORAGE_KEY = "pokemon-data";
+const LOCAL_STORAGE_KEY_COMPLETED = "pokemon-data-completed";
+let caughtCount = 0;
+
 const SPRITE_CLASSES = [
-    'pkmn__snake',
-    'pkmn__quadruped',
-    'pkmn__rhydon',
-    'pkmn__bug',
-    'pkmn__plant',
-    'pkmn__bird',
-    'pkmn__clefairy',
-    'pkmn__ball',
-    'pkmn__fossil',
-    'pkmn__pikachu',
-    'pkmn__aquatic'
+  "pkmn__snake",
+  "pkmn__quadruped",
+  "pkmn__rhydon",
+  "pkmn__bug",
+  "pkmn__plant",
+  "pkmn__bird",
+  "pkmn__clefairy",
+  "pkmn__ball",
+  "pkmn__fossil",
+  "pkmn__pikachu",
+  "pkmn__aquatic",
 ];
 
 const getRandomizedData = (dataArr) => {
@@ -22,19 +25,34 @@ const getLocalStorageData = () =>
 const setLocalStorageData = (data) =>
   localStorage.setItem(LOCAL_STORAGE_KEY, data);
 
+const getLocalStorageCompletedData = () =>
+  localStorage.getItem(LOCAL_STORAGE_KEY_COMPLETED)?.split(",");
+const addToLocalStorageCompletedData = (newItem) => {
+  const existingCompleted = getLocalStorageCompletedData();
+  const data =
+    existingCompleted && existingCompleted[0]
+      ? [...existingCompleted, newItem]
+      : [newItem];
+  localStorage.setItem(LOCAL_STORAGE_KEY_COMPLETED, data);
+};
+const resetLocalStorageCompletedData = () => {
+  localStorage.setItem(LOCAL_STORAGE_KEY_COMPLETED, []);
+};
+
 const shiftLocalStorageData = () => {
   const data = getLocalStorageData();
   data.shift();
   setLocalStorageData(data);
 };
 
-const getRandomSprite = () => SPRITE_CLASSES[Math.floor(Math.random() * SPRITE_CLASSES.length)];
+const getRandomSprite = () =>
+  SPRITE_CLASSES[Math.floor(Math.random() * SPRITE_CLASSES.length)];
 
 const switchSprites = () => {
-    const spritesElArr = document.getElementsByClassName("pkmn");
-    spritesElArr.item(0).classList = `pkmn ${getRandomSprite()}`;
-    spritesElArr.item(1).classList = `pkmn ${getRandomSprite()}`;
-}
+  const spritesElArr = document.getElementsByClassName("pkmn");
+  spritesElArr.item(0).classList = `pkmn ${getRandomSprite()}`;
+  spritesElArr.item(1).classList = `pkmn ${getRandomSprite()}`;
+};
 
 const setNextState = () => {
   const dataArr = getLocalStorageData();
@@ -59,6 +77,17 @@ const resetData = () => {
   setLocalStorageData(randomizedData);
   document.getElementById("btnNext").removeAttribute("disabled");
   document.getElementById("btnSkip").removeAttribute("disabled");
+
+  resetLocalStorageCompletedData();
+  document.getElementById("caughtPokemon").innerHTML = [];
+
+  caughtCount = 0;
+  document.getElementById("caughtCount").innerHTML = caughtCount;
+};
+
+const setCaughtCount = (count) => {
+  caughtCount = count;
+  document.getElementById("caughtCount").innerHTML = caughtCount;
 };
 
 const setup = () => {
@@ -66,20 +95,38 @@ const setup = () => {
   if (!data) {
     resetData();
   }
+  const completedData = getLocalStorageCompletedData();
+  if (completedData && completedData[0]) {
+    setCaughtCount(completedData.length);
+    completedData.forEach(
+      (data) =>
+        (document.getElementById(
+          "caughtPokemon"
+        ).innerHTML += `<span>${data}</span>`)
+    );
+  } else {
+    setCaughtCount(0);
+  }
 
   document.getElementById("btnNext").addEventListener("click", () => {
-    const [_toRemove, ...data] = getLocalStorageData();
+    const [caughtPokemon, ...data] = getLocalStorageData();
     setLocalStorageData(data);
+    setNextState();
+    addToLocalStorageCompletedData(caughtPokemon);
+
+    document.getElementById(
+      "caughtPokemon"
+    ).innerHTML += `<span>${caughtPokemon}</span>`;
+    setCaughtCount(caughtCount + 1);
+  });
+
+  document.getElementById("btnSkip").addEventListener("click", () => {
+    shiftLocalStorageData();
     setNextState();
   });
 
   document.getElementById("btnReset").addEventListener("click", () => {
     resetData();
-    setNextState();
-  });
-
-  document.getElementById("btnSkip").addEventListener("click", () => {
-    shiftLocalStorageData();
     setNextState();
   });
 };
